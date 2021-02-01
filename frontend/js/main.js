70b35126b3afe6c1cb2c59d537bc599743016382
Tom Texier <tom.texierpro@gmail.com>
2021-02-01 16:37:51 +0100

    Homepage - Liste des produits

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,45 @@
 getAllCameras = async () => {
-    let data = await fetch('http://localhost:3000/api/cameras')
+    await fetch('http://localhost:3000/api/cameras')
         .then(response => response.json())
+        .then(response => setLayoutCameras(response))
         .catch(error => alert("Erreur : " + error));
-
-    return data;
 };
 
-const cameras = getAllCameras();
-console.log(cameras);
+setLayoutCameras = async (cameras) => {
+    const productList = document.getElementById("product-list");
+    cameras.forEach(camera => {
+        let article = document.createElement("article");
+        let action = document.createElement("a");
+        let image = document.createElement("div");
+
+        let content = document.createElement("section");
+        let title = document.createElement("h3");
+        let description = document.createElement("p");
+
+        article.classList.add("product");
+
+        action.setAttribute("href", "./product.html?id=" + camera._id);
+
+        image.classList.add("image");
+
+        image.style.background = "url(" + camera.imageUrl + ") no-repeat center/cover";
+
+        content.classList.add("content");
+        title.innerText = camera.name;
+        description.innerText = cutString(camera.description, 80);
+
+        productList.append(article);
+        article.append(action);
+        action.append(image);
+        image.append(content)
+        content.append(title, description);
+    });
+}
+
+cutString = (str, max_length) => {
+    let new_str = str.substring(0, max_length);
+    if(str.length > max_length) {
+        new_str += "...";
+    }
+     return new_str;
+}
